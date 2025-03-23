6bc972c7c87f774d673d3e1f587faff822b015a7
evkov17 <kovalevs2404@gmail.com>
2025-03-23 16:33:28 +0200

    added .o and .a file

--- lab08/prj/Kovalova_task/main.cpp
+++ lab08/prj/Kovalova_task/main.cpp
@@ -2,9 +2,22 @@
 #include <iomanip>
 #include <cmath>
 #include <windows.h>
-#include "ModulesKovalova.h"
 
 using namespace std;
+
+double s_calculation(double x, double y, double z) {
+    // Перевірка ділення на нуль
+    if (fabs(y) < 1e-10) {
+        return NAN;
+    }
+
+    double denominator = y * y * y;
+    double sinPart = z * sin(x * x * y);
+    double sqrtArg = fabs(z - 12 * x);
+    double sqrtPart = sqrt(sqrtArg);
+
+    return sinPart + sqrtPart / denominator;
+}
 
 void copyright_info() {
     cout << "Ковальова Єва ©, Усі права захищені." << endl;
