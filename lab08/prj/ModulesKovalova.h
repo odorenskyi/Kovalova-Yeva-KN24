b9d0177a4657ad3cfc8f3cd083d54f7207b292a9
evkov17 <kovalevs2404@gmail.com>
2025-04-16 19:16:50 +0300

    added new .h file

--- lab08/prj/ModulesKovalova.h
+++ lab08/prj/ModulesKovalova.h
@@ -7,4 +7,8 @@ void shoes(float sizes);
 int binary_count(int N);
 void task_selector(char option);
 
+void analyze_text_file(const std::string& input_file, const std::string& output_file);
+void append_punctuation_and_timestamp(const std::string& input_file);
+void append_results_to_file(const std::string& output_file, double x, double y, double z, int b);
+
 #endif // MODULESKOVALOVA_H_INCLUDED
